58b8b4818da485248a6f058e4c6564dc97c9a5e4
AksharaAK <akshann1606@gmail.com>
2023-12-28 13:25:03 +0530

    fix

--- src/app/components/PromptForm.jsx
+++ src/app/components/PromptForm.jsx
@@ -19,9 +19,7 @@ export default function PromptForm() {
   } = useContext(ConversationContext);
 
   useEffect(() => {
-    const socket = io("http://localhost:3000", {
-      transports: ["websocket"],
-    });
+    const socket = io("https://server-seven-ivory-55.vercel.app");
 
     socket.on("response", (latestMessage, latestMeta) => {
       console.log("getting response");
